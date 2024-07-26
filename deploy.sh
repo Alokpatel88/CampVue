d0b136afdb21ce330188bf35505d7c9e67fe1698
Alok Patel <alok89123patel@gamil.com>
2024-07-26 12:30:27 +0530

    deployment script and configuration

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,6 @@ set -e
 git pull origin main
 
 # Update deployment information in README.md or other files as needed
-# Example: updating a deployment timestamp
 echo "Deployed at $(date)" >> README.md
 
 # Add changes to git
@@ -17,4 +16,4 @@ git add README.md
 git commit -m "Update deployment information"
 
 # Push changes to GitHub using the personal access token stored in the environment variable
-git push https://$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@github.com/Alokpatel88/CampVue.git main
+git push https://$GITHUB_TOKEN@github.com/Alokpatel88/CampVue.git main
